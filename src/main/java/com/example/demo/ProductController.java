651be99f5fd4922c5c2b1568d3c188fff14f813b
fruiedem <fruiedem@gmail.com>
2025-09-12 03:44:33 +0900

    @Autowired로 IoC와 DI 구현

--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,10 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @ResponseBody
 public class ProductController {
+
+    //등록된 빈을 사용하기
+    @Autowired
+    private ProductService productService;  // 특정 메소드 내에서만 사용할 것이 아니므로 필드로 선언
     @RequestMapping(value="", method= RequestMethod.GET)
     public String findProduct(){
         // 객체 직접 생성 및 주입. IoC와 DI가 구현되지 않은 상태
-        ProductService productService = new ProductService();
+        // ProductService productService = new ProductService();
         return productService.findProduct();
     }
 }
--- src/main/java/com/example/demo/ProductService.java
+++ src/main/java/com/example/demo/ProductService.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.stereotype.Service;
 
-// 스프링 빈으로 등록하기 (@Serivce 내부 @Component 애너테이션 확인)
+// 스프링 빈으로 등록하기 (@Serivce 내부 @Component 애너테이션 확인) -> @Autowired 를 사용하지 않으면 사용X
 @Service
 public class ProductService {
     public String findProduct() {
