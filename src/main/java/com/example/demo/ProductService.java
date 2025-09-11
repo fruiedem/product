0f63672cbee6de63eb6e1dfda66d4babc1587bb9
fruiedem <fruiedem@gmail.com>
2025-09-12 04:58:24 +0900

    생성자 주입으로 DI 구현

--- src/main/java/com/example/demo/ProductService.java
+++ src/main/java/com/example/demo/ProductService.java
@@ -6,8 +6,12 @@ import org.springframework.stereotype.Service;
 // 스프링 빈으로 등록하기 (@Serivce 내부 @Component 애너테이션 확인) -> @Autowired 를 사용하지 않으면 사용X
 @Service
 public class ProductService {
-    @Autowired
+    // 생성자 주입하여 DI 구현 방법  
     private ProductRepository productRepository;
+    @Autowired
+    ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
     public String findProduct() {
         return productRepository.findProduct();
     }
