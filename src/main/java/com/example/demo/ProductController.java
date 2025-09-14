2a061a596b650c9c416e8cc8b5182a3ce637101e
fruiedem <fruiedem@gmail.com>
2025-09-14 12:30:01 +0900

    rest api url 규칙

--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -13,10 +13,15 @@ public class ProductController {
     //등록된 빈을 사용하기
     @Autowired
     private ProductService productService;  // 특정 메소드 내에서만 사용할 것이 아니므로 필드로 선언
-    @RequestMapping(value="", method= RequestMethod.GET)
+    @RequestMapping(value="/products", method= RequestMethod.GET)
     public String findProduct(){
         // 객체 직접 생성 및 주입. IoC와 DI가 구현되지 않은 상태
         // ProductService productService = new ProductService();
         return productService.findProduct();
+    }
+
+    @RequestMapping(value="/products", method=RequestMethod.POST)
+    public void saveProduct(){
+        productService.saveProduct();
     }
 }
